5d551eff2cf45f9c0ef60b8781ba50cb81ff4dc3
Sam Dahl <samldahl@gmail.com>
2026-02-02 11:53:40 -0600

    add BookShelf component and Initial state. intitial state for newBook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 // src/App.jsx
+import './App.css';
+import Bookshelf from './components/Bookshelf/Bookshelf.jsx';
+
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const Bookshelf = () => {
+  const [books, setBooks] = useState([
+    { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+    { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+  ]);
+
+  const [newBook, setNewBook] = useState({
+    title: '',
+    author: ''
+  });
+
+  return (
+    <div className="bookshelfDiv">
+      <div className="formDiv">
+        <h3>Add a Book</h3>
+        {/* Form will go here */}
+      </div>
+      <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+    </div>
+  );
+};
+
+export default Bookshelf;
